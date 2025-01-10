1d4738b66c43ae22bdb4b03b62bcd8db005273ed
EthanSmithson <ethansmithson413@gmail.com>
2025-01-10 20:08:34 +0000

    contact form validation and email sending

--- src/main/java/com/example/chatapp/controllers/ContactMeController.java
+++ src/main/java/com/example/chatapp/controllers/ContactMeController.java
@@ -1,6 +1,7 @@
 package com.example.chatapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.DynamicRegistrationBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.chatapp.services.ContactMeService;
+import com.example.chatapp.services.*;
 
+import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,8 +26,13 @@ public class ContactMeController {
 	@PostMapping("/contactMe")
 		public Object ContactMe(@RequestBody Map<String, String> formData) {
 
-			System.out.println(contactMeService.sendEmail(formData));
-			
-			return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+			Map<String, Boolean> data = contactMeService.validate(formData);
+			if(data.values().contains(false)) {
+				// return ResponseEntity.ok(data);
+				return new ResponseEntity<>(data, HttpStatus.OK);
+			} else {
+				// contactMeService.sendEmail(formData);
+				return new ResponseEntity<>(data, HttpStatus.OK);
+			}
 		}
 }
--- src/main/java/com/example/chatapp/services/ContactMeService.java
+++ src/main/java/com/example/chatapp/services/ContactMeService.java
@@ -1,17 +1,71 @@
 package com.example.chatapp.services;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Pattern;
 
-// Importing required classes
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class ContactMeService {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
     public boolean sendEmail(Map<String, String> data) {
+        SimpleMailMessage message = new SimpleMailMessage();
         String name = (String) data.get("name");
         String email = (String) data.get("email");
         String body = (String) data.get("body");
+        message.setTo("ethansmithson413@gmail.com");
+        message.setFrom(email);
+        message.setSubject(name);
+        message.setText(body);
         
+        mailSender.send(message);
+
         return true;
     }
+
+    public Map<String, Boolean> validate(Map<String, String> data) {
+        String name = (String) data.get("name");
+        String email = (String) data.get("email");
+        String body = (String) data.get("body");
+        Boolean nameBad = true;
+        Boolean emailBad = true;
+        Boolean bodyBad = true;
+        if (name == null) {
+            nameBad = false;
+        }
+        if (!isValidEmail(email)) {
+            emailBad = false;
+        }
+        if (body == null) {
+            bodyBad = false;
+        }
+
+        Map<String, Boolean> validate = new Hashtable<>();
+        validate.put("nameValid", nameBad);
+        validate.put("emailValid", emailBad);
+        validate.put("bodyValid", bodyBad);
+
+        return validate;
+    }
+
+    private static boolean isValidEmail(String email) {
+        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
 }
